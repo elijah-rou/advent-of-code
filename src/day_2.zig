2b42e694bb504af0120bc9e19f1cbc55edd06416
Elijah Roussos <elijah.rou@gmail.com>
2023-12-02 14:15:30 +0200

    simplify code

--- src/day_2.zig
+++ src/day_2.zig
@@ -2,27 +2,41 @@ const std = @import("std");
 const util = @import("util.zig");
 const ArrayList = std.ArrayList;
 
-const RED = 12;
-const GREEN = 13;
-const BLUE = 14;
-
 fn is_digit(char: u8) bool {
-    if (char >= 48 and char < 58) {
-        return true;
-    }
-    return false;
+    return char >= '0' and char <= '9';
 }
 
-fn get_cubes(results: std.StringHashMap(?[]u8), key: []const u8) i32 {
-    const amount = results.get(key).?;
-    if (amount != null) {
-        return std.fmt.parseInt(i32, amount.?, 10) catch {
-            return 0;
-        };
-    } else {
-        return 0;
+const Result = struct {
+    const Self = @This();
+    const RED_MAX = 12;
+    const GREEN_MAX = 13;
+    const BLUE_MAX = 14;
+
+    red: i32 = 0,
+    green: i32 = 0,
+    blue: i32 = 0,
+
+    fn remax(self: *Self, new_colours: Result) void {
+        self.red = @max(new_colours.red, self.red);
+        self.green = @max(new_colours.green, self.green);
+        self.blue = @max(new_colours.blue, self.blue);
     }
-}
+
+    fn power(self: *Self) i32 {
+        return self.red * self.green * self.blue;
+    }
+
+    fn is_invalid(self: *Self) bool {
+        if (self.red > RED_MAX) {
+            return true;
+        } else if (self.green > GREEN_MAX) {
+            return true;
+        } else if (self.blue > BLUE_MAX) {
+            return true;
+        }
+        return false;
+    }
+};
 
 pub fn main() !void {
     const part = std.os.argv[1][0];
@@ -36,60 +50,46 @@ pub fn main() !void {
     var game_power_sum: i32 = 0;
     while (lines.next()) |game| {
         var details = std.mem.splitScalar(u8, game, ':');
-        const id = details.next().?;
-        const game_id = if (is_digit(id[id.len - 2])) id[id.len - 2 .. id.len] else id[id.len - 1 .. id.len];
+        const game_id = details.next().?[5..];
+        const gameplay = details.next().?;
 
-        const draws = details.next().?;
-        var hands = std.mem.splitScalar(u8, draws, ';');
-        var invalid: bool = false;
-        var red_max: i32 = 0;
-        var green_max: i32 = 0;
-        var blue_max: i32 = 0;
-
-        while (hands.next()) |set| {
-            var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
-            defer _ = arena.deinit();
-            const allocator = arena.allocator();
-            var results = std.StringHashMap(?[]u8).init(allocator);
-            try results.put("red", null);
-            try results.put("green", null);
-            try results.put("blue", null);
-
+        var bag_draws = std.mem.splitScalar(u8, gameplay, ';');
+        var result = Result{};
+        var invalid_game = false;
+        while (bag_draws.next()) |set| {
             var sets = std.mem.splitScalar(u8, set, ',');
+            var draw_result = Result{};
             while (sets.next()) |cubes| {
+                var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+                defer _ = arena.deinit();
+                const allocator = arena.allocator();
                 var amount = ArrayList(u8).init(allocator);
                 for (cubes) |char| {
                     if (is_digit(char)) {
                         try amount.append(char);
-                    } else if (char == 114) {
-                        try results.put("red", amount.items);
+                    } else if (char == 'r') {
+                        draw_result.red = try std.fmt.parseInt(i32, amount.items, 10);
                         break;
-                    } else if (char == 103) {
-                        try results.put("green", amount.items);
+                    } else if (char == 'g') {
+                        draw_result.green = try std.fmt.parseInt(i32, amount.items, 10);
                         break;
-                    } else if (char == 98) {
-                        try results.put("blue", amount.items);
+                    } else if (char == 'b') {
+                        draw_result.blue = try std.fmt.parseInt(i32, amount.items, 10);
                         break;
                     }
                 }
+                if (draw_result.is_invalid()) {
+                    invalid_game = true;
+                }
             }
-            const red_amount = get_cubes(results, "red");
-            const green_amount = get_cubes(results, "green");
-            const blue_amount = get_cubes(results, "blue");
-
-            if (red_amount > RED or green_amount > GREEN or blue_amount > BLUE) {
-                invalid = true;
-            }
-            red_max = @max(red_amount, red_max);
-            green_max = @max(green_amount, green_max);
-            blue_max = @max(blue_amount, blue_max);
+            result.remax(draw_result);
         }
-        if (!invalid) {
+        if (!invalid_game) {
             // std.log.debug("Take: {s}", .{game_id});
             game_id_sum += try std.fmt.parseInt(i32, game_id, 10);
         }
         // std.log.debug("{d}, {d}, {d}", .{ red_max, green_max, blue_max });
-        game_power_sum += (red_max * green_max * blue_max);
+        game_power_sum += result.power();
     }
     std.log.info("Id Sum: {d}", .{game_id_sum});
     std.log.info("Power Sum: {d}", .{game_power_sum});
