4382a2a0b239b27915e94425db0c4761d15c490b
Elijah Roussos <elijah.rou@gmail.com>
2023-12-03 21:15:08 +0200

    cleanup

--- src/day_3.zig
+++ src/day_3.zig
@@ -10,11 +10,12 @@ fn check_part(part: u8) EnginePart {
         return .gear;
     } else if (util.is_digit(part) or part == 46) {
         return .none;
+    } else {
+        return .part;
     }
-    return .part;
 }
 
-fn check_surrounds(engine: [][]u8, y_max: usize, y: usize, x_max: usize, x: usize) struct { part: EnginePart, x: usize, y: usize } {
+fn check_surrounds(engine: [][]const u8, y_max: usize, y: usize, x_max: usize, x: usize) struct { part: EnginePart, x: usize, y: usize } {
     var part: EnginePart = .none;
     if (y > 0 and x > 0) {
         part = check_part(engine[y - 1][x - 1]);
@@ -67,11 +68,11 @@ fn check_surrounds(engine: [][]u8, y_max: usize, y: usize, x_max: usize, x: usiz
     return .{ .part = part, .x = 0, .y = 0 };
 }
 
-fn update_accumulators(part_type: *EnginePart, part_accumulator: *u32, gear_accumulator: *u32, current_part: *ArrayList(u8), current_gear: [2]usize, gears: *HashMap([2]usize, u32)) !void {
-    if (part_type.* != .none) {
-        const part_value = try std.fmt.parseInt(u32, current_part.items, 10);
+fn update_accumulators(current_part: *EnginePart, part_accumulator: *u32, gear_accumulator: *u32, current_value: *ArrayList(u8), current_gear: [2]usize, gears: *HashMap([2]usize, u32)) !void {
+    if (current_part.* != .none) {
+        const part_value = try std.fmt.parseInt(u32, current_value.items, 10);
         part_accumulator.* += part_value;
-        if (part_type.* == .gear) {
+        if (current_part.* == .gear) {
             var gear_value = gears.get(current_gear);
             if (gear_value == null) {
                 try gears.put(current_gear, part_value);
@@ -80,8 +81,8 @@ fn update_accumulators(part_type: *EnginePart, part_accumulator: *u32, gear_accu
             }
         }
     }
-    part_type.* = .none;
-    current_part.clearAndFree();
+    current_part.* = .none;
+    current_value.clearAndFree();
 }
 
 pub fn main() !void {
@@ -89,14 +90,10 @@ pub fn main() !void {
     const ma = main_arena.allocator();
     defer _ = main_arena.deinit();
 
-    var grid = ArrayList([]u8).init(ma);
+    var grid = ArrayList([]const u8).init(ma);
     var lines = try util.read_delim(&ma, "resources/day_3/input", "\n");
     while (lines.next()) |line| {
-        var components = ArrayList(u8).init(ma);
-        for (line) |char| {
-            try components.append(char);
-        }
-        try grid.append(try components.toOwnedSlice());
+        try grid.append(line);
     }
     const engine = try grid.toOwnedSlice();
     const y_dim = engine.len;
@@ -105,27 +102,27 @@ pub fn main() !void {
     std.log.info("Grid x size: {d}", .{x_dim});
 
     var part_accumulator: u32 = 0;
+    var current_part: EnginePart = .none;
+    var current_value = ArrayList(u8).init(ma);
     var gear_accumulator: u32 = 0;
-    var part_type: EnginePart = .none;
-    var current_part = ArrayList(u8).init(ma);
     var gears = HashMap([2]usize, u32).init(ma);
     var current_gear: [2]usize = .{ undefined, undefined };
     for (engine, 0..) |col, y| {
         for (col, 0..) |char, x| {
             if (util.is_digit(char)) {
-                try current_part.append(char);
-                if (part_type == .none) {
+                try current_value.append(char);
+                if (current_part == .none) {
                     const part_details = check_surrounds(engine, y_dim - 1, y, x_dim - 1, x);
-                    part_type = part_details.part;
-                    if (part_type == .gear) {
+                    current_part = part_details.part;
+                    if (current_part == .gear) {
                         current_gear = .{ part_details.x, part_details.y };
                     }
                 }
             } else {
-                try update_accumulators(&part_type, &part_accumulator, &gear_accumulator, &current_part, current_gear, &gears);
+                try update_accumulators(&current_part, &part_accumulator, &gear_accumulator, &current_value, current_gear, &gears);
             }
         }
-        try update_accumulators(&part_type, &part_accumulator, &gear_accumulator, &current_part, current_gear, &gears);
+        try update_accumulators(&current_part, &part_accumulator, &gear_accumulator, &current_value, current_gear, &gears);
     }
     std.log.info("Part Sum: {d}", .{part_accumulator});
     std.log.info("Gears: {d}", .{gears.count()});
