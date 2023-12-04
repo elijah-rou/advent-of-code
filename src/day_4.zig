1d815e098af1232fb79d13fda2ba998080d129a4
Elijah Roussos <elijah.rou@gmail.com>
2023-12-04 11:22:06 +0200

    nearly done part 2

--- src/day_4.zig
+++ src/day_4.zig
@@ -1,25 +1,22 @@
 const std = @import("std");
 const util = @import("util.zig");
-const Set = std.AutoHashMap(u32, void);
+const HashMap = std.AutoHashMap(u32, u32);
+const Set = std.StringHashMap(void);
 const Allocator = std.mem.Allocator;
 
 fn scratch_draw(allocator: Allocator, score_list: []const u8) !Set {
-    // std.log.debug("{s}", .{score_list});
     var str_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
     var winning_scores = Set.init(allocator);
-    while (str_scores.next()) |s| {
-        // std.log.debug("{s}", .{s});
-        const score = try std.fmt.parseInt(u32, s, 10);
+    while (str_scores.next()) |score| {
         try winning_scores.put(score, undefined);
     }
     return winning_scores;
 }
 
-fn get_points(winning_scores: Set, score_list: []const u8) !u32 {
-    var str_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
+fn am_i_lucky(winning_scores: Set, score_list: []const u8) !u32 {
+    var draw_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
     var total_score: u32 = 0;
-    while (str_scores.next()) |s| {
-        const score = try std.fmt.parseInt(u32, s, 10);
+    while (draw_scores.next()) |score| {
         if (winning_scores.contains(score)) {
             total_score = switch (total_score) {
                 0 => 1,
@@ -30,10 +27,19 @@ fn get_points(winning_scores: Set, score_list: []const u8) !u32 {
     return total_score;
 }
 
-fn am_i_lucky(allocator: Allocator, scorecard: []const u8) !u32 {
-    var score_lists = std.mem.splitScalar(u8, scorecard, '|');
-    const winning_scores = try scratch_draw(allocator, score_lists.next().?);
-    return try get_points(winning_scores, score_lists.next().?);
+fn scratchcard_bonanza(card: u32, winning_scores: Set, card_collection: *HashMap, score_list: []const u8) !void {
+    var draw_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
+    var count: u32 = 1;
+    while (draw_scores.next()) |score| {
+        if (winning_scores.contains(score)) {
+            const received_card = card + count;
+            if (card_collection.get(received_card)) |value| {
+                try card_collection.put(received_card, value + 1);
+            } else {
+                try card_collection.put(received_card, 1);
+            }
+        }
+    }
 }
 
 pub fn main() !void {
@@ -41,12 +47,29 @@ pub fn main() !void {
     const ma = main_arena.allocator();
     defer _ = main_arena.deinit();
 
-    var lines = try util.read_delim(&ma, "resources/day_4/input", "\n");
+    var lines = try util.read_delim(&ma, "resources/day_4/test", "\n");
     var total_score: u32 = 0;
+
+    var card_collection = HashMap.init(ma);
+    var card_idx: u32 = 1;
     while (lines.next()) |line| {
         if (line.len > 0) {
-            total_score += try am_i_lucky(ma, line[9..]);
+            var score_lists = std.mem.splitScalar(u8, line[9..], '|');
+            const winning_scores = try scratch_draw(ma, score_lists.next().?);
+            const draw_scores = score_lists.next().?;
+
+            total_score += try am_i_lucky(winning_scores, draw_scores);
+            try scratchcard_bonanza(card_idx, winning_scores, &card_collection, draw_scores);
         }
+        card_idx += 1;
     }
+
+    var total_cards: u32 = 0;
+    var card_iterator = card_collection.valueIterator();
+    while (card_iterator.next()) |card_number| {
+        total_cards += card_number.*;
+    }
+
     std.log.info("Score: {d}", .{total_score});
+    std.log.info("Total Cards: {d}", .{total_cards});
 }
