0aad9ffeb128472e9837646b55bc0d125561f1fe
Elijah Roussos <elijah.rou@gmail.com>
2023-12-04 12:40:46 +0200

    merge am_i_luck and scratchcard bonanza

--- src/day_4.zig
+++ src/day_4.zig
@@ -4,29 +4,6 @@ const HashMap = std.AutoArrayHashMap(u32, u32);
 const Set = std.StringHashMap(void);
 const Allocator = std.mem.Allocator;
 
-fn scratch_draw(allocator: Allocator, score_list: []const u8) !Set {
-    var str_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
-    var winning_scores = Set.init(allocator);
-    while (str_scores.next()) |score| {
-        try winning_scores.put(score, undefined);
-    }
-    return winning_scores;
-}
-
-fn am_i_lucky(winning_scores: Set, score_list: []const u8) !u32 {
-    var draw_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
-    var total_score: u32 = 0;
-    while (draw_scores.next()) |score| {
-        if (winning_scores.contains(score)) {
-            total_score = switch (total_score) {
-                0 => 1,
-                else => total_score * 2,
-            };
-        }
-    }
-    return total_score;
-}
-
 fn add_card(card: u32, card_collection: *HashMap, card_amount: u32) !void {
     if (card_collection.get(card)) |value| {
         try card_collection.put(card, value + card_amount);
@@ -35,18 +12,25 @@ fn add_card(card: u32, card_collection: *HashMap, card_amount: u32) !void {
     }
 }
 
-fn scratchcard_bonanza(card: u32, winning_scores: Set, card_collection: *HashMap, score_list: []const u8) !void {
+fn scratchcard_bonanza(card: u32, winning_scores: Set, card_collection: *HashMap, score_list: []const u8) !u32 {
     var draw_scores = std.mem.tokenizeScalar(u8, score_list, ' ');
     try add_card(card, card_collection, 1);
 
     const current_card_count = card_collection.get(card).?;
     var matches: u32 = 1;
+    var total_score: u32 = 0;
     while (draw_scores.next()) |score| {
         if (winning_scores.contains(score)) {
+            total_score = switch (total_score) {
+                0 => 1,
+                else => total_score * 2,
+            };
             try add_card(card + matches, card_collection, current_card_count);
             matches += 1;
         }
     }
+
+    return total_score;
 }
 
 pub fn main() !void {
@@ -61,12 +45,17 @@ pub fn main() !void {
     var card_idx: u32 = 1;
     while (lines.next()) |line| {
         if (line.len > 0) {
-            var score_lists = std.mem.splitScalar(u8, line[9..], '|');
-            const winning_scores = try scratch_draw(ma, score_lists.next().?);
-            const draw_scores = score_lists.next().?;
+            var loop_arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+            const la = loop_arena.allocator();
+            defer _ = loop_arena.deinit();
 
-            total_score += try am_i_lucky(winning_scores, draw_scores);
-            try scratchcard_bonanza(card_idx, winning_scores, &card_collection, draw_scores);
+            var score_lists = std.mem.splitScalar(u8, line[9..], '|');
+            var draw_scores = std.mem.tokenizeScalar(u8, score_lists.next().?, ' ');
+            var winning_scores = Set.init(la);
+            while (draw_scores.next()) |score| {
+                try winning_scores.put(score, undefined);
+            }
+            total_score += try scratchcard_bonanza(card_idx, winning_scores, &card_collection, score_lists.next().?);
         }
         card_idx += 1;
     }
