cabab27ed4c782434b03fa31399e09fd33bea618
Elijah Roussos <elijah.rou@gmail.com>
2023-12-05 22:43:12 +0200

    yay acceptable time

--- src/day_5.zig
+++ src/day_5.zig
@@ -82,8 +82,8 @@ pub fn main() !void {
     const ma = main_arena.allocator();
     defer _ = main_arena.deinit();
 
-    var lines = try util.read_delim(&ma, "resources/day_5/test", "\n");
-    var seeds = std.mem.splitScalar(u8, lines.next().?[7..], ' ');
+    var lines = try util.read_delim(&ma, "resources/day_5/input", "\n");
+    var seed_list = std.mem.splitScalar(u8, lines.next().?[7..], ' ');
     var maps = [7]SeedArray{ undefined, undefined, undefined, undefined, undefined, undefined, undefined };
 
     var idx: usize = 0;
@@ -104,25 +104,59 @@ pub fn main() !void {
     }
 
     var min_loc: i64 = 1000000000000000000; // BEEG number
-    while (seeds.next()) |s| {
-        switch (PART) {
-            1 => {
-                const seed = try std.fmt.parseInt(i64, s, 10);
-                const location = seed_map(maps, seed);
+    switch (PART) {
+        1 => {
+            while (seed_list.next()) |s| {
+                var location = try std.fmt.parseInt(i64, s, 10);
+                for (maps) |map| {
+                    for (map.items) |seed_func| {
+                        if (location >= seed_func.start and location < seed_func.end) {
+                            location += seed_func.function;
+                            break;
+                        }
+                    }
+                }
                 if (location < min_loc) {
                     min_loc = location;
                 }
-            },
-            else => {
-                const seed_start = try std.fmt.parseInt(i64, s, 10);
-                const seed_range = try std.fmt.parseInt(i64, seeds.next().?, 10);
-                const location = try seed_map_range(maps, seed_start, seed_start + seed_range);
-                if (location < min_loc) {
-                    min_loc = location;
+            }
+        },
+        else => {
+            var seeds = ArrayList([2]i64).init(ma);
+            while (seed_list.next()) |s| {
+                const range_start = try std.fmt.parseInt(i64, s, 10);
+                const seed_range = try std.fmt.parseInt(i64, seed_list.next().?, 10);
+                try seeds.append(.{ range_start, range_start + seed_range });
+            }
+            for (maps) |map| {
+                var ranges = ArrayList([2]i64).init(ma);
+                while (seeds.popOrNull()) |seed| {
+                    for (map.items) |range| {
+                        const max_start = max(seed[0], range.start);
+                        const min_end = min(seed[1], range.end);
+                        if (max_start < min_end) {
+                            try ranges.append(.{ max_start + range.function, min_end + range.function });
+                            if (max_start > seed[0]) {
+                                try seeds.append(.{ seed[0], max_start });
+                            }
+                            if (min_end < seed[1]) {
+                                try seeds.append(.{ min_end, seed[1] });
+                            }
+                            break;
+                        }
+                    } else {
+                        try ranges.append(.{ seed[0], seed[1] });
+                    }
                 }
-            },
-        }
+                seeds.clearAndFree();
+                seeds = ranges;
+            }
+            for (seeds.items) |range| {
+                if (range[0] < min_loc) {
+                    min_loc = range[0];
+                }
+            }
+        },
     }
-
     std.log.info("Lowest Loc Num: {d}", .{min_loc});
 }
