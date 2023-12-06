7a0d734a3597651adde659f76c78f762754d751f
Elijah Roussos <elijah.rou@gmail.com>
2023-12-06 11:17:52 +0200

    change day 6 to quadratic formula

--- src/day_6.zig
+++ src/day_6.zig
@@ -38,7 +38,7 @@ pub fn main() !void {
         try time_strs.append(t);
     }
     const big_time_str = try concat(ma, time_strs);
-    const big_time = try std.fmt.parseUnsigned(usize, big_time_str, 10);
+    const big_time = try std.fmt.parseFloat(f64, big_time_str);
     time_strs.clearAndFree();
 
     var distances = ArrayList(usize).init(ma);
@@ -49,14 +49,14 @@ pub fn main() !void {
         try distance_strs.append(d);
     }
     const big_record_str = try concat(ma, distance_strs);
-    const big_record = try std.fmt.parseUnsigned(usize, big_record_str, 10);
+    const big_record = try std.fmt.parseFloat(f64, big_record_str);
     distance_strs.clearAndFree();
 
+    // Part 1: Brute Force
     var record_mult: u32 = 1;
     for (times.items, distances.items) |time, record| {
         if (time != 0) {
             var record_times: u32 = 0;
-
             for (1..time) |velocity| {
                 const distance = velocity * (time - velocity);
                 if (distance > record) {
@@ -67,13 +67,16 @@ pub fn main() !void {
         }
     }
 
-    var big_record_wins: u128 = 0;
-    for (1..big_time) |velocity| {
-        const distance = velocity * (big_time - velocity);
-        if (distance > big_record) {
-            big_record_wins += 1;
-        }
-    }
+    // Part 2: Quadratic Formula
+    // distance = (time - velocity) * velocity
+    // 0 = velocity^2 - velocity*time + distance
+    // solve for velocity, time = -big_time, distance = big_record
+    // velocity = (-time±√(time²-4distance))/2
+    // record must equal max(velocity) - min(velocity)
+    const inner_term = std.math.sqrt(big_time * big_time - 4 * big_record);
+    const solution_1 = std.math.floor((big_time + inner_term) / 2);
+    const solution_2 = std.math.ceil((big_time - inner_term) / 2);
+
     std.log.info("Record Mult: {d}", .{record_mult});
-    std.log.info("Big Race Records: {d}", .{big_record_wins});
+    std.log.info("Big Race Record: {d}", .{solution_1 - solution_2 + 1});
 }
